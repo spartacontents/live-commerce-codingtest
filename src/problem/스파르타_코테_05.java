f8abf5be27149c70310789843daf2820bb7fe3c7
김희성 <heesung.kim@42dot.ai>
2025-11-16 23:11:27 +0900

    문제 5번 샘플 코드 추가

--- src/problem/스파르타_코테_05.java
+++ src/problem/스파르타_코테_05.java
@@ -68,7 +68,7 @@ public class 스파르타_코테_05 {
      */
 
     public static void main(String[] args) throws IOException {
-
+        System.out.println(solution(5, 5, new int[][]{{0, 2, 1, 1, 1}, {0, 10, 8, 100, 1}, {1, 10, 7, 100, 1}, {2, 10, 6, 100, 10}, {3, 4, 5, 100, 0}}));
     }
 
     /*
